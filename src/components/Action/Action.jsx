46dee88486cfad53906e52cdddf0ce428abe1ec2
[TUF] AlexGamer29 <sun123vet@gmail.com>
2024-01-27 23:12:30 +0700

    fix download link

--- src/components/Action/Action.jsx
+++ src/components/Action/Action.jsx
@@ -1,15 +1,31 @@
 import { React } from 'react'
+import axios from 'axios'
+import fileDownload from 'js-file-download'
+import { getImageName } from '../../utils/'
 
 function Action(props) {
   const { Icon, Download } = props
 
+  const handleDownload = (url, filename) => {
+    axios
+      .get(url, {
+        responseType: 'blob',
+      })
+      .then((res) => {
+        fileDownload(res.data, filename)
+      })
+  }
+
   if (Icon.render.name === 'ArrowDownIcon') {
     return (
-      <a aria-label="download-link" href={Download} download>
-        <div className="p-3 bg-slate-50 rounded-md cursor-pointer shadow-md group">
-          <Icon className="w-4 h-4 text-neutral-500 group-hover:text-neutral-800" />
-        </div>
-      </a>
+      <div
+        className="p-3 bg-slate-50 rounded-md cursor-pointer shadow-md group"
+        onClick={() => {
+          handleDownload(Download, getImageName(Download).concat('.jpeg'))
+        }}
+      >
+        <Icon className="w-4 h-4 text-neutral-500 group-hover:text-neutral-800" />
+      </div>
     )
   }
   return (
--- src/data/data.js
+++ src/data/data.js
@@ -6,95 +6,214 @@ const posts = [
       'https://images.unsplash.com/profile-fb-1545878736-f4f19f671e1a.jpg?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
     title: 'City',
     photo:
-      'http://localhost:4566/visionary-img/1706247821738_%5BChat%20App%5D%20Database%20Diagram.png',
+      'https://images.unsplash.com/photo-1653397663005-ff23af371a0f?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0OHx8fGVufDB8fHx8&auto=format&fit=crop&w=500',
   },
   {
     id: '2',
-    username: 'Howen',
+    username: 'Nicholas Ng',
     image:
-      'https://images.unsplash.com/profile-fb-1545878736-f4f19f671e1a.jpg?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
-    title: 'City',
+      'https://images.unsplash.com/profile-1629183370299-87eadd08ff76?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    title: 'Camera',
     photo:
-      'http://localhost:4566/visionary-img/1706249485612_1706001867729_file_419909695_1071034534120247_786283346350110351_n.jpg',
+      'https://images.unsplash.com/photo-1653389151473-d1f6a29216d5?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1NHx8fGVufDB8fHx8&auto=format&fit=crop&w=500',
   },
   {
     id: '3',
-    username: 'Howen',
+    username: 'Rob Potter',
     image:
-      'https://images.unsplash.com/profile-fb-1545878736-f4f19f671e1a.jpg?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
-    title: 'City',
+      'https://images.unsplash.com/profile-1615471170607-5b69efaf98d4image?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    title: 'Bird',
     photo:
-      'http://localhost:4566/visionary-img/1706249545960_alex-nguyen-OFz0X16hGuQ-unsplash.jpg',
+      'https://images.unsplash.com/photo-1653388678184-383ba9b93229?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1N3x8fGVufDB8fHx8&auto=format&fit=crop&w=500',
   },
   {
     id: '4',
-    username: 'Howen',
+    username: 'Alex Ware',
     image:
-      'https://images.unsplash.com/profile-fb-1545878736-f4f19f671e1a.jpg?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
-    title: 'City',
-    photo: 'http://localhost:4566/visionary-img/1706249553992_conve.png',
+      'https://images.unsplash.com/profile-1607965916560-93ac86739000image?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    title: 'skyscraper',
+    photo:
+      'https://images.unsplash.com/photo-1653322232484-71c8eb863727?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5N3x8fGVufDB8fHx8&auto=format&fit=crop&w=500',
   },
   {
     id: '5',
-    username: 'Howen',
+    username: 'Klim Musalimov',
     image:
-      'https://images.unsplash.com/profile-fb-1545878736-f4f19f671e1a.jpg?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
-    title: 'City',
+      'https://images.unsplash.com/profile-1651083033201-1894e4576de7image?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    title: 'Watch',
     photo:
-      'http://localhost:4566/visionary-img/1706249568034_photo-anime-1692931134739.jpg',
+      'https://images.unsplash.com/photo-1653376382561-ed6a1feeb79b?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMTd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500',
   },
   {
     id: '6',
-    username: 'Howen',
+    username: 'Skyler Ewing',
     image:
-      'https://images.unsplash.com/profile-fb-1545878736-f4f19f671e1a.jpg?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
-    title: 'City',
-    photo: 'http://localhost:4566/visionary-img/1706249577362_IMG_9698.jpeg',
+      'https://images.unsplash.com/profile-1652281779153-a5d035d154b9image?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    title: 'Landscape',
+    photo:
+      'https://images.unsplash.com/photo-1652534607016-68c37239402a?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500',
   },
   {
     id: '7',
-    username: 'Howen',
+    username: 'Muhammad Fawdy',
     image:
-      'https://images.unsplash.com/profile-fb-1545878736-f4f19f671e1a.jpg?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
-    title: 'City',
+      'https://images.unsplash.com/profile-1653287653281-26f7f2d14677image?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    title: 'Breakfast',
     photo:
-      'http://localhost:4566/visionary-img/1706249588071_zesan-h-jkFNkNfwzvc-unsplash.jpg',
+      'https://images.unsplash.com/photo-1653286015985-d4857eac5679?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500',
   },
   {
     id: '8',
-    username: 'Howen',
+    username: 'Pawan Thapa',
     image:
-      'https://images.unsplash.com/profile-fb-1545878736-f4f19f671e1a.jpg?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
-    title: 'City',
+      'https://images.unsplash.com/profile-1628793405840-dd0694da4d8aimage?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    title: 'Landscape',
     photo:
-      'http://localhost:4566/visionary-img/1706249599484_334745144_1235938870348501_5652480241816994682_n.jpg',
+      'https://images.unsplash.com/photo-1653161926627-c4b492a300c9?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNTN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500',
   },
   {
     id: '9',
-    username: 'Howen',
+    username: 'Jimmy Woo',
     image:
-      'https://images.unsplash.com/profile-fb-1545878736-f4f19f671e1a.jpg?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
-    title: 'City',
+      'https://images.unsplash.com/profile-1647388905298-95b0b34d10bd?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    title: 'Train',
     photo:
-      'http://localhost:4566/visionary-img/1706249610667_DroneShot-Youtube.png',
+      'https://images.unsplash.com/photo-1653312763062-e8b12913db98?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNzJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500',
   },
   {
     id: '10',
-    username: 'Howen',
+    username: 'Pawan Thapa',
     image:
-      'https://images.unsplash.com/profile-fb-1545878736-f4f19f671e1a.jpg?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
-    title: 'City',
+      'https://images.unsplash.com/profile-1628793405840-dd0694da4d8aimage?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    title: 'Sunset',
     photo:
-      'http://localhost:4566/visionary-img/1706249634712_44 CHỢ HOA TẾT 2023 - QUẢNG TRƯỜNG 2_9 ĐÀ NẴNG - CÂY ME 550 TRIỆU.png',
+      'https://images.unsplash.com/photo-1653161652704-d53d7130d718?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxODR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500',
   },
   {
     id: '11',
-    username: 'Howen',
+    username: 'Skyler Ewing',
     image:
-      'https://images.unsplash.com/profile-fb-1545878736-f4f19f671e1a.jpg?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
-    title: 'City',
+      'https://images.unsplash.com/profile-1652281779153-a5d035d154b9image?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    title: 'Dog',
     photo:
-      'http://localhost:4566/visionary-img/1706249655277_320517533_470104835280295_5270533637629602517_n.jpg',
+      'https://images.unsplash.com/photo-1652489997199-4d80abd175db?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzM0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500',
+  },
+  {
+    id: '12',
+    username: 'Sergey Pesterev',
+    image:
+      'https://images.unsplash.com/profile-fb-1481053706-12c6f2403ad3.jpg?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    title: 'Winter',
+    photo:
+      'https://images.unsplash.com/photo-1653311236202-aa57d07e0b9c?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxODl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500',
+  },
+  {
+    id: '13',
+    username: 'Gaman Alice',
+    image:
+      'https://images.unsplash.com/profile-1647388905298-95b0b34d10bd?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    title: 'Sign',
+    photo:
+      'https://images.unsplash.com/photo-1653226166190-442adb1b1691?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyOTl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500',
+  },
+  {
+    id: '14',
+    username: 'Edwin Yang',
+    image:
+      'https://images.unsplash.com/profile-1652980182731-8476492cb89e?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    title: 'Desk',
+    photo:
+      'https://images.unsplash.com/photo-1652957465310-a5c2cfb1d844?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMTd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500',
+  },
+  {
+    id: '15',
+    username: 'Filipe Coimbra',
+    image:
+      'https://images.unsplash.com/profile-1604069342756-1f57384ed553image?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    title: 'Canteen',
+    photo:
+      'https://images.unsplash.com/photo-1651809171850-4c8b2afd5462?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNTd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500',
+  },
+  {
+    id: '16',
+    username: 'Colin + Meg',
+    image:
+      'https://images.unsplash.com/profile-1647388905298-95b0b34d10bd?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    title: 'River',
+    photo:
+      'https://images.unsplash.com/photo-1653184972233-c5292037ddb3?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0MDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500',
+  },
+  {
+    id: '17',
+    username: 'Ali Choubin',
+    image:
+      'https://images.unsplash.com/profile-1636371880454-6761217c3351image?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    title: 'Coffee',
+    photo:
+      'https://images.unsplash.com/photo-1653085315275-3a1ce8e25ae3?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0NzV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500',
+  },
+  {
+    id: '18',
+    username: 'Albulena Panduri',
+    image:
+      'https://images.unsplash.com/profile-1653076582680-041ec762ba33image?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    title: 'Moon',
+    photo:
+      'https://images.unsplash.com/photo-1653076362668-a717e53fe8f9?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0ODh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500',
+  },
+  {
+    id: '19',
+    username: 'Subhadeep Saha',
+    image:
+      'https://images.unsplash.com/profile-1627498860642-dfcee890fd6eimage?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    title: 'Night Sky',
+    photo:
+      'https://images.unsplash.com/photo-1653069282619-fccdd12bf97a?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0OTN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500',
+  },
+  {
+    id: '20',
+    username: 'Josh Hild',
+    image:
+      'https://images.unsplash.com/profile-1652754804106-1b1e8b7c0528image?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    title: 'Road',
+    photo:
+      'https://images.unsplash.com/photo-1652972756954-5a02a1eaa5cd?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3NTR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500',
+  },
+  {
+    id: '21',
+    username: 'Timo Wielink',
+    image:
+      'https://images.unsplash.com/profile-1469548965216-0fb27c4659d8?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    title: 'Newyork',
+    photo:
+      'https://images.unsplash.com/photo-1652865060241-35ea9dd54bc2?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5Mjh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500',
+  },
+  {
+    id: '22',
+    username: '1MilliDollars',
+    image:
+      'https://images.unsplash.com/profile-1627498860642-dfcee890fd6eimage?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    title: 'Basketball',
+    photo:
+      'https://images.unsplash.com/photo-1652815569705-86b21ed6a62c?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMTA3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500',
+  },
+  {
+    id: '23',
+    username: 'Tobias Reich',
+    image:
+      'https://images.unsplash.com/profile-1627236157452-63b4b9400a64image?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    title: 'Downtown',
+    photo:
+      'https://images.unsplash.com/photo-1652794878130-d7274e14e244?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMTM1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500',
+  },
+  {
+    id: '24',
+    username: 'Nick Gosset',
+    image:
+      'https://images.unsplash.com/profile-1652446832582-ad4f149d593dimage?dpr=1&auto=format&fit=crop&w=32&h=32&q=60&crop=faces&bg=fff',
+    title: 'Boat',
+    photo:
+      'https://images.unsplash.com/photo-1652447061965-203cffceceda?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMjA1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500',
   },
 ];
 
