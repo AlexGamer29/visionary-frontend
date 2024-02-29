b2438b0db2e04ad5694d29099dfff9c17085e1eb
[TUF] AlexGamer29 <sun123vet@gmail.com>
2024-02-29 15:15:45 +0700

    add image viewer

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -3,42 +3,80 @@ import Flicking, { ViewportSlot } from '@egjs/react-flicking'
 import { Arrow } from '@egjs/flicking-plugins'
 import '@egjs/flicking-plugins/dist/arrow.css'
 import '@egjs/react-flicking/dist/flicking-inline.css'
+import './Carousel.css'
+import Zoom from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
 import posts from '../../data/data'
 
 export default class Carousel extends Component {
   constructor(props) {
     super(props)
     this.plugins = [new Arrow()]
+    this.state = {
+      currentIndex: 0,
+    }
   }
 
-  renderPosts() {
+  renderPosts = () => {
     return posts.map((post) => (
-      <div
-        key={post.id}
-        className="card-panel"
-        style={{
-          minWidth: 'min(100%, 426px)',
-          maxWidth: 'calc(73.3815vh - 128.418px)',
-        }}
-      >
-        <img
-          src={post.photo}
-          alt=""
+      <div>
+        <div
+          key={post.id}
+          className="card-panel"
           style={{
-            height: '100%',
+            height: '95%',
           }}
-        />
+        >
+          <Zoom classDialog="custom-zoom">
+            <img src={post.photo} alt={post.title} loading="lazy" />
+          </Zoom>
+        </div>
+        <div
+          id="hehe"
+          key={post.photo}
+          style={{
+            height: '5%',
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          {post.title}
+        </div>
       </div>
     ))
   }
 
+  handleChange = (e) => {
+    this.setState({ currentIndex: e.index })
+  }
+
   render() {
     return (
-      <Flicking circular={true} plugins={this.plugins}>
+      <Flicking
+        onChanged={(e) => console.log(e.index)}
+        circular={true}
+        plugins={this.plugins}
+      >
         {this.renderPosts()}
         <ViewportSlot>
-          <span className="flicking-arrow-prev"></span>
-          <span className="flicking-arrow-next"></span>
+          <span
+            className="flicking-arrow-prev is-thin"
+            style={{
+              position: 'fixed',
+              top: '50%',
+              right: 0,
+              transform: 'translateY(-50%)',
+            }}
+          ></span>
+          <span
+            className="flicking-arrow-next is-thin"
+            style={{
+              position: 'fixed',
+              top: '50%',
+              right: 0,
+              transform: 'translateY(-50%)',
+            }}
+          ></span>
         </ViewportSlot>
       </Flicking>
     )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,76 @@
 import React, { useState } from 'react'
+import { Modal } from 'antd'
+import { createStyles, useTheme } from 'antd-style'
 import { Layout, Header, Main } from '../../layouts'
 import { Hero, Collection, Carousel } from '../../components'
+import posts from '../../data/data'
+
+const useStyle = createStyles(({ token }) => ({
+  'my-modal-body': {
+    // background: token.blue1,
+    // overflow: 'auto',
+    // marginTop: token.marginLG,
+  },
+  'my-modal-mask': {
+    boxShadow: `inset 0 0 15px #fff`,
+  },
+  'my-modal-header': {
+    // borderBottom: `1px dotted ${token.colorPrimary}`,
+  },
+  'my-modal-footer': {
+    color: token.colorPrimary,
+  },
+  'my-modal-content': {
+    border: '1px solid #333',
+    boxShadow: '0 0 30px #999',
+    maxHeight:
+      'calc(1000vh - 200px)' /* Adjust the max height based on your layout */,
+    overflow: 'auto' /* Allow scrolling if content overflows */,
+  },
+}))
 
 function Home() {
   const [showCarousel, setShowCarousel] = useState(false)
+  const { styles } = useStyle()
+  const token = useTheme()
 
   const toggleCarousel = () => {
     setShowCarousel(!showCarousel)
+  }
+
+  const handleOk = () => {
+    setShowCarousel(false)
+  }
+
+  const handleCancel = () => {
+    setShowCarousel(false)
+  }
+
+  const classNames = {
+    body: styles['my-modal-body'],
+    mask: styles['my-modal-mask'],
+    header: styles['my-modal-header'],
+    footer: styles['my-modal-footer'],
+    content: styles['my-modal-content'],
+  }
+
+  const modalStyles = {
+    header: {
+      borderLeft: `5px solid ${token.colorPrimary}`,
+      borderRadius: 0,
+      paddingInlineStart: 5,
+    },
+    body: {
+      // boxShadow: 'inset 0 0 5px #999',
+      borderRadius: 5,
+      margin: '1rem',
+    },
+    mask: {
+      // backdropFilter: 'blur(10px)',
+    },
+    content: {
+      boxShadow: '0 0 30px #999',
+    },
   }
 
   return (
@@ -15,7 +79,26 @@ function Home() {
       <Main>
         <Hero />
         <Collection toggleCarousel={toggleCarousel} />
-        {showCarousel && <Carousel />}
+        <Modal
+          style={{
+            overflow: 'auto',
+            backfaceVisibility: 'hidden',
+            cursor: 'zoom-out',
+            inset: '0 0 0 0',
+            position: 'fixed',
+            zIndex: 999,
+            scrollbarWidth: 'none',
+          }}
+          classNames={classNames}
+          styles={modalStyles}
+          open={showCarousel}
+          onOk={handleOk}
+          onCancel={handleCancel}
+          width={'80%'}
+        >
+          <Carousel data={posts} />
+          {/* <Carousel /> */}
+        </Modal>
       </Main>
     </Layout>
   )
